369250651c8c10bc13e01438b15dd55c7080bb6f
Ivan Raffucci <Ivan Raffucci>
2017-04-04 00:12:32 -0700

    landing page to revamp

--- Revamp/javascript/home-js.js
+++ Revamp/javascript/home-js.js
@@ -1,17 +1,3 @@
-function showContact() {
-  document.getElementsByClassName("contact")[0].style.display = "flex";
-  document.getElementsByClassName("contact")[0].style.animation = "showit 500ms forwards";
-}
-function hideContact() {
-
-  document.getElementsByClassName("contact")[0].style.display = "none";
-  document.getElementsByClassName("contact")[0].style.animation = "";
-}
-
-function grab(element) {
-  element.style.cursor = "-webkit-grabbing";
-}
-
-function stopGrab(element) {
-  element.style.cursor = "pointer";
+function scrollToBio() {
+  $("body").animate({ scrollTop:$(window).height()*1.02}, 1500);
 }
