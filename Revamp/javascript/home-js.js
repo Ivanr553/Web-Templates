1625997e57060cb98985b6cbf06330f7cccc036b
Ivan Raffucci <Ivan Raffucci>
2017-04-08 19:40:23 -0700

    finished about me and working on applications

--- Revamp/javascript/home-js.js
+++ Revamp/javascript/home-js.js
@@ -1,5 +1,5 @@
 function scrollToBio() {
-  $("body").animate({ scrollTop: $(".section-1-1").offset().top}, 1500);
+  $("body").animate({ scrollTop: $(".section-1").offset().top}, 1500);
 }
 
 function scrollToWebsites() {
@@ -26,13 +26,17 @@ function openTokiwa() {
   window.open("http://www.ivanraffucci.tech/Tokiwa/index.html");
 }
 
+function openRaincheck() {
+  window.open("https://frozen-springs-75857.herokuapp.com/");
+}
+
 function backToTop() {
   $("body").animate({ scrollTop: 0}, 2500)
 }
 
 function allowOverflow(element) {
   if(element.style.overflowY == "hidden") {
-  element.style.overflowY = "scroll";
+    element.style.overflowY = "scroll";
   }
   else {
     element.style.overflowY = "hidden";
@@ -40,7 +44,7 @@ function allowOverflow(element) {
 }
 
 $(window).scroll(function() {
-  let scroll = $(window).height();
+  let wHeight = $(window).height();
   let scrollT = $(this).scrollTop();
 
     $(".grey-box-bottom").css({
@@ -50,16 +54,20 @@ $(window).scroll(function() {
       "transform" : "translate(0px, -"+ scrollT /10 +"px)"
     })
 
-
-  if($(window).scrollTop() > scroll*0.5) {
-    $("#back-to-top").css({"display" : "flex"});
+  if(scrollT < wHeight*1.4) {
+    $("#back-to-top").css({
+      "display" : "none"
+    });
   }
-  if($(window).scrollTop() > scroll*1.6) {
+  if(scrollT > wHeight*1.4) {
+    $("#back-to-top").css({
+      "display" : "flex",
+      "animation" : "fade-in 1s forwards"
+    });
+  }
+  if(scrollT > wHeight*1.9) {
     $("#carla").css({"animation" : "slide-in-left 1.5s forwards"});
     $("#tokiwa").css({"animation" : "slide-in-right 1.5s forwards"});
-  }
-  else {
-    $("#back-to-top").css({"display" : "none"});
   }
 })
 
@@ -68,3 +76,66 @@ $(".about-me-span").each(function() {
     $("about-me-span").css("font-size") = 12;
   }
 })
+
+// DRAGGABLE FUNCTIONALITY FOR APPLICATION BUBBLES
+// $($(".bubble").each(function() {
+//   const leftC = Math.floor($(this).parent().width())/2;
+//   const leftP = (Math.floor($(this).position().left)) + (Math.floor($(this).height()/2));
+//   const leftD = (leftC - leftP) + "px";
+//   const topC = Math.floor($(this).parent().height())/2;
+//   const topP = (Math.floor($(this).position().top)) + (Math.floor($(this).width()/2));
+//   const topD = (topC - topP) + "px";
+//   $(this).draggable();
+//   $(this).mouseup(function(){$(this).css({
+//     "animation" : "return 2s forwards"
+//   })})
+//   $(this).mousedown(function() {
+//     console.log(leftD, topD);
+//     $(this).css({
+//       "animation" : "none",
+//       "left" : leftD,
+//       "top" : topD
+//     })
+//   })
+// })
+// )
+
+
+function openApps() {
+  $("#application-title").css({
+    "animation" : "slide-up 4s forwards",
+    "position" : "absolute"
+  })
+  $(".bubble").each(function() {
+    $(this).css({
+      "animation" : "fade-in-bubble 4.7s forwards",
+      "display" : "flex",
+    })
+  })
+ setTimeout(function() {addHoverAnim()}, 3200);
+}
+function addHoverAnim() {
+  $(".bubble-container").css({
+    "animation" : "show-back 500ms forwards"
+  })
+
+}
+
+function openRaincheckInfo() {
+  $(".information-container").each(function() {
+    $(this).css({
+      "display" : "flex",
+      "animation" : "open-up 1.5s forwards"
+    })
+  })
+  $(".bubble-wrap").each(function() {
+    $(this).css({
+      "animation" : "move-left 1.5s forwards"
+    })
+  })
+  $(".raincheck-information").each(function() {
+    $(this).css({
+      "animation" : "fade-in-info 2.5s forwards"
+    })
+  })
+}
