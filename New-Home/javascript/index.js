ff988ea013475d6d5ec6dafd57e4c1a768d19f12
Ivan Raffucci <ivanr553@gmail.com>
2017-07-08 20:55:59 -0700

    good progress on new website

---
+++ New-Home/javascript/index.js
@@ -0,0 +1,45 @@
+//Functions to change display and link in rojects section
+function showCarla() {
+  $("#display").css({
+    "background-image" : "url('./images/carla.jpg')"
+  })
+  document.getElementById("display").setAttribute("onclick", "openCarla()")
+}
+
+function showJk() {
+  $("#display").css({
+    "background-image" : "url('./images/jk.jpg')"
+  })
+  document.getElementById("display").setAttribute("onclick", "openJk()")
+}
+
+function showTokiwa() {
+  $("#display").css({
+    "background-image" : "url('./images/tokiwa.jpg')"
+  })
+  document.getElementById("display").setAttribute("onclick", "openTokiwa()")
+}
+
+function showRaincheck() {
+  $("#display").css({
+    "background-image" : "url('./images/raincheck.jpg')"
+  })
+  document.getElementById("display").setAttribute("onclick", "openRaincheck()")
+}
+
+//Function for links
+function openCarla() {
+  window.open("https://carlakunkelweb.github.io/website/")
+}
+
+function openJk() {
+  console.log("WIP")
+}
+
+function openTokiwa() {
+  window.open("http://www.ivanraffucci.tech/Tokiwa/index.html")
+}
+
+function openRaincheck() {
+  window.open("https://tranquil-chamber-53541.herokuapp.com/rainchecks")
+}
